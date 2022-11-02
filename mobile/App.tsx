6754327276ade1d410b5651468e0870d3bf20f0c
kramer <kramerscs@gmail.com>
2022-11-02 13:10:24 -0300

    Adding screens

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,20 +4,27 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
+
+import { AuthContextProvider } from './src/contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Center flex={1} bgColor="gray.900">
-        {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Center flex={1} bgColor="gray.900">
+          {fontsLoaded ? <Pools /> : <Loading />}
 
-      </Center>
+        </Center>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
